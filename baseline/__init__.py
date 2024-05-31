fec470afb805ccd041f1d3a4255cc0175d93b211
shengangxiong <33759328+shengangxiong@users.noreply.github.com>
2024-05-31 17:23:59 +0800

    Delete baseline directory

--- baseline/__init__.py
+++
@@ -1,4 +0,0 @@
-from criterion.distiller_zoo.ft_based import *
-from criterion.distiller_zoo.le_based import *
-from criterion.distiller_zoo.logits_based import *
-from criterion.distiller_zoo.base_distiller import BaseDistiller
